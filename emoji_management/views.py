ccdf58173c34d4c6fc918cf4204d9089deb28ac5
ihcnay <2435459530@qq.com>
2024-10-04 10:15:03 +0800

    10.4_10:14

--- emoji_management/views.py
+++ emoji_management/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib import auth, messages
+from .models import USER_TO_CAPACITY
 
 
 def user_login(request):
@@ -36,6 +37,9 @@ def register(request):
                 user = User.objects.create_user(username=username, password=password, email=email)
                 user.save()
                 if user:
+                    capacity = request.POST.get('capacity')
+                    user_capacity = USER_TO_CAPACITY.objects.create(user=user, capacity=capacity)
+                    user_capacity.save()
                     messages.success(request, '注册成功！')
                     return redirect('login')
     return render(request, 'register.html')
@@ -44,3 +48,27 @@ def register(request):
 def home(request):
     username = request.user.username
     return render(request, 'home.html', {'username': username})
+
+
+def send_page(request):             ##转到发送界面，可选择表情和课堂号等
+    pass
+
+
+def send_emoji(request):            ##
+    pass
+
+
+def history_page(request):           ##历史查询页面，根据课堂号查询历史表情
+    pass
+
+
+def view_history(request):
+    pass
+
+
+def export_page(request):             ##导出数据界面，选择导出方式
+    pass
+
+
+def export_to_xxx(request):
+    pass
