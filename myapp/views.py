36f33dfd12e38eb3b0a51d0af4d14ccf884cc535
user1315492038 <1315492038@qq.com>
2024-12-07 11:07:07 +0800

    12.7

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect ,get_object_or_404
 from django.contrib import auth, messages
 from myapp.models import *
 from django.http import JsonResponse
